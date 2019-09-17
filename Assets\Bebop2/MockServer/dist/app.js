e098fccd51a79a401a6731740936e14d7a6d5e4f
ken <ken.ahn@nsuslab.com>
2019-09-17 16:13:39 +0900

    Revert "no message"
    This reverts commit 122d4e73c3519bd8cb51903985e2235acb609d8a.

--- Assets\Bebop2/MockServer/dist/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const appServer_1 = require("./server/appServer");
-let app = new appServer_1.AppServer().getApp();
-exports.app = app;
-//# sourceMappingURL=app.js.map
